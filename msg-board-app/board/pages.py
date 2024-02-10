f559f01ca50f9f81231e09866a1e21963da5321b
cliffordEmmanuel <cliffemmanuels@gmail.com>
2024-02-10 15:06:30 +0000

    version 1 of messaging board app

---
+++ msg-board-app/board/pages.py
@@ -0,0 +1,16 @@
+"""This is playing the role of views in the MVC architecture
+"""
+
+from flask import Blueprint, render_template
+
+bp = Blueprint("pages", __name__)
+
+
+@bp.route("/")
+def home():
+    return render_template("pages/home.html")
+
+
+@bp.route("/about")
+def about():
+    return render_template("pages/about.html")
